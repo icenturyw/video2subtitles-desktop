7799c1b65f3e928d93db68d658c665935c4b528d
centuryw <icenturyw@gmail.com>
2026-05-26 22:39:07 +0800

    启用 GPU 设置补丁

--- app.py
+++ app.py
@@ -27,9 +27,11 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QFont
 from main_window import MainWindow, apply_theme
 import settings_patch
+import gpu_patch
 import output_patch
 
 settings_patch.install()
+gpu_patch.install()
 output_patch.install()
 
 
